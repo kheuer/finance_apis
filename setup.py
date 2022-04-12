543630e3109cf400ff985c23e8c125f61863bd60
kheuer <83609198+kheuer@users.noreply.github.com>
2022-04-12 19:24:51 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name="finance_apis",
+    version="1.0.0",
+    description="Api classes to serve data from financial apis",
+    author="kheuer",
+    author_email="",
+    url="",
+    packages=["finance_apis"],
+    install_requires=["distutils", "json", "datetime", "requests", "time"]
+    )
+
