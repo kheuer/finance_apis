1510d2e090969edd016e5517bfb45fd80dc0f32a
kheuer <83609198+kheuer@users.noreply.github.com>
2022-04-13 10:20:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 setup(name="finapi",
     version="v1.0.0",
     description="Api classes to serve data from financial apis",
     author="kheuer",
-    author_email="",
-    url=""
+    packages=find_packages()
     )
 
